d07c553923eb0f0d10bf811532ae59723a49b2be
Jonas Karlsson <96485472+kalsson@users.noreply.github.com>
2025-02-02 13:21:56 +0100

    Part 9, Add validation
    Added data annotations to the Movie model for validation, including length constraints, required fields, and regular expressions for Genre and Rating. Updated database migrations to reflect these constraints. Adjusted seed data to comply with the new validation rules.

--- MvcMovie.kalsson/MvcMovie/Models/SeedData.cs
+++ MvcMovie.kalsson/MvcMovie/Models/SeedData.cs
@@ -22,7 +22,7 @@ public class SeedData
                     Title = "When Harry Met Sally",
                     ReleaseDate = DateTime.Parse("1989-2-12"),
                     Genre = "Romantic Comedy",
-                    Rating = "R",
+                    Rating = "PG",
                     Price = 7.99M
                 },
                 new Movie
@@ -30,7 +30,7 @@ public class SeedData
                     Title = "Ghostbusters ",
                     ReleaseDate = DateTime.Parse("1984-3-13"),
                     Genre = "Comedy",
-                    Rating = "R",
+                    Rating = "PG",
                     Price = 8.99M
                 },
                 new Movie
@@ -38,7 +38,7 @@ public class SeedData
                     Title = "Ghostbusters 2",
                     ReleaseDate = DateTime.Parse("1986-2-23"),
                     Genre = "Comedy",
-                    Rating = "R",
+                    Rating = "PG",
                     Price = 9.99M
                 },
                 new Movie
@@ -46,7 +46,7 @@ public class SeedData
                     Title = "Rio Bravo",
                     ReleaseDate = DateTime.Parse("1959-4-15"),
                     Genre = "Western",
-                    Rating = "R",
+                    Rating = "PG",
                     Price = 3.99M
                 }
             );
